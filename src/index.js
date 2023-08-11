44a6259a414794f763f0d8b815d96fe007319364
YuliyaVoronovich <Y-1306@mail.ru>
2023-08-11 18:36:52 +0300

    feat: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let bracketsMap = new Map(bracketsConfig);
+
+  const bracketStack = [];
+
+  for (let i = 0; i < str.length; i++) {
+   
+
+   if (str[i] === bracketsMap.get(bracketStack[bracketStack.length - 1])) {    
+    bracketStack.pop();
+
+    } else {
+      bracketStack.push(str[i]);
+    }
+  }
+  return bracketStack.length === 0;
 }
