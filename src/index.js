57dbbdb4e459d905b03d8c883b242abf0dcbd924
YuliyaVoronovich <Y-1306@mail.ru>
2023-08-14 11:38:44 +0300

    refactor: code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
 
   let bracketsMap = new Map(bracketsConfig);
-
   const bracketStack = [];
 
   for (let i = 0; i < str.length; i++) {
    
+    (str[i] === bracketsMap.get(bracketStack[bracketStack.length - 1])) ? bracketStack.pop() : bracketStack.push(str[i]);
 
-   if (str[i] === bracketsMap.get(bracketStack[bracketStack.length - 1])) {    
-    bracketStack.pop();
-
-    } else {
-      bracketStack.push(str[i]);
-    }
   }
   return bracketStack.length === 0;
 }
